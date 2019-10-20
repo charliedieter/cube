89e7bdf4ca22908e7e2f9826ab4266bfc5b9aa45
Charlie Dieter <cdieter@jwplayer.com>
2019-10-20 14:45:40 -0400

    Formatting

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,40 +1,40 @@
-import { combineReducers } from "redux";
-import { createTiles } from "../util/cubeUtils";
+import { combineReducers } from 'redux'
+import { createTiles } from '../util/cubeUtils'
 
 const cube = (oldState = createTiles(), action) => {
   switch (action.type) {
-    case "SET_CHANGES":
-      return action.newCube;
+    case 'SET_CHANGES':
+      return action.newCube
     default:
-      return oldState;
+      return oldState
   }
-};
+}
 
 const queue = (oldState = [], action) => {
   switch (action.type) {
-    case "ENQUEUE":
-      return [...action.moves, ...oldState];
-    case "SET_CHANGES":
-      return oldState.slice(0, -1);
-    case "END_SHUFFLE":
-      return [];
+    case 'ENQUEUE':
+      return [...oldState, ...action.moves]
+    case 'SET_CHANGES':
+      return oldState.slice(0, -1)
+    case 'END_SHUFFLE':
+      return []
     default:
-      return oldState;
+      return oldState
   }
-};
+}
 
 const shuffling = (oldState = false, action) => {
   switch (action.type) {
-    case "START_SHUFFLE":
-      return true;
-    case "END_SHUFFLE":
-      return false;
+    case 'START_SHUFFLE':
+      return true
+    case 'END_SHUFFLE':
+      return false
     default:
-      return oldState;
+      return oldState
   }
-};
+}
 
 export default combineReducers({
   entities: combineReducers({ cube, queue }),
-  ui: combineReducers({ shuffling })
-});
+  ui: combineReducers({ shuffling }),
+})
--- src/util/colors.js
+++ src/util/colors.js
@@ -1,8 +1,8 @@
 export default {
-  f: "#0000ff", // blue
-  b: "#00cc00", // green 
-  u: "white",
-  d: "#ff0000", // red
-  l: "#ff9900", // orange
-  r: "#ffff00" // yellow
-};
+  f: '#0000ff', // blue
+  b: '#00cc00', // green
+  u: 'white',
+  d: '#ff0000', // red
+  l: '#ff9900', // orange
+  r: '#ffff00', // yellow
+}
