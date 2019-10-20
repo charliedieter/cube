4de1459d55494bc63c303f285b7f83a7c0787a4c
Charlie Dieter <cdieter@jwplayer.com>
2019-10-20 14:44:14 -0400

    Formatting

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react'
+import { Link } from 'react-router-dom'
 import Cube from '../components/Cube'
+import Moves from '../components/Moves'
 
-export default function () {
+function Home() {
   return (
     <main className="home">
       <h2 style={{ fontSize: '3rem', margin: '0 auto' }}>cube</h2>
-      <h1 style={{ margin: '0 auto 1rem' }}>A simple Rubik's cube tutorial.</h1>
-      <Link className="control" to="/tutorial">get started</Link>
+      <h1 style={{ margin: '0 auto 1rem' }}>
+        A simple Rubik's cube tutorial (work in progress).
+      </h1>
+      <Link className="control" to="/tutorial" style={{ color: '#fff' }}>
+        get started
+      </Link>
       <Cube spin />
     </main>
   )
 }
+
+export default Home
