9bb92120d26d37b9a5a35dddc7ef589da4f761ee
Charlie Dieter <cdieter@jwplayer.com>
2019-10-20 14:47:19 -0400

    Updates

---
+++ precache-manifest.54454a9ac1af740b010812e0c612b608.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0eeef38c535ce3ec8af89bd6a5e9c676",
+    "url": "/cube/index.html"
+  },
+  {
+    "revision": "445abc52ed47382eb345",
+    "url": "/cube/static/css/main.ffc1db3d.chunk.css"
+  },
+  {
+    "revision": "5bd2c00b5abb5c5b6629",
+    "url": "/cube/static/js/2.26b29c8e.chunk.js"
+  },
+  {
+    "revision": "445abc52ed47382eb345",
+    "url": "/cube/static/js/main.58f38452.chunk.js"
+  },
+  {
+    "revision": "4958bc15bf40a3a59547",
+    "url": "/cube/static/js/runtime-main.e6ab25b6.js"
+  }
+]);
