e06a305ae26f27ecb0ea8ee0a1fe12bd8138600c
Charlie Dieter <cdieter@jwplayer.com>
2019-10-20 14:43:41 -0400

    Refactor slice to work with new move names and structure

--- src/components/Slice/Slice.js
+++ src/components/Slice/Slice.js
@@ -1,69 +1,59 @@
-import React from "react";
-import { connect } from "react-redux";
-import { setChanges } from "../../util/cubeUtils";
-import MOVEMENTS from "../../util/movements";
+import React from 'react'
+import { connect } from 'react-redux'
+import { setChanges } from '../../util/cubeUtils'
+import MOVEMENTS from '../../util/movements'
 
-function Slice({ children, axis, movement, cube, dispatch, shuffling }) {
-  let direction = movement.split("-")[1];
-
-  if (axis === "y" || axis === "z") {
-    direction = direction === "left" ? "right" : "left";
-  }
-  const duration = shuffling ? "0.175s" : ".4s";
+function Slice({
+  children,
+  axis,
+  direction,
+  movement,
+  cube,
+  dispatch,
+  shuffling,
+}) {
+  const duration = shuffling ? '0.175s' : '.4s'
 
   return (
     <div
       className="slice"
       style={{
-        transformStyle: "preserve-3d",
-        transformOrigin: "203px 203px",
-        animation: `SpinningSlice-${direction}-${axis} ${duration}`
+        transformStyle: 'preserve-3d',
+        transformOrigin: '203px 203px',
+        animation: `SpinningSlice-${direction}-${axis} ${duration}`,
       }}
       onAnimationEnd={() =>
         dispatch({
-          type: "SET_CHANGES",
-          newCube: setChanges(cube, movement)
+          type: 'SET_CHANGES',
+          newCube: setChanges(cube, movement),
         })
       }
     >
       {children}
     </div>
-  );
+  )
 }
 
 const msp = ({ entities: { cube }, ui: { shuffling } }) => ({
   cube,
-  shuffling
-});
+  shuffling,
+})
 
-const ConnectedSlice = connect(msp)(Slice);
-
-function getAxis(movement) {
-  if (movement === "down-right") {
-    return "x";
-  } else if (
-    movement === "sideways-left" ||
-    movement === "sideways-right" ||
-    movement === "bottomtwo-left"
-  ) {
-    return "y";
-  }
-  return movement[1] === "E" ? "y" : movement[1] === "S" ? "z" : "x";
-}
+const ConnectedSlice = connect(msp)(Slice)
 
 const renderMove = (cube, allTiles, queue) => {
-  const movement = queue.pop(); // this needs to be done a more redux-ey way (without mutation), and debounce between inputs
-  const toSlice = Object.keys(MOVEMENTS[movement].moves);
-  const slicedTiles = allTiles.filter(({ key }) => toSlice.includes(key));
-  const unslicedTiles = allTiles.filter(({ key }) => !toSlice.includes(key));
-  const axis = getAxis(movement);
+  const movement = queue[queue.length - 1] //FIXME: race condition here, for some reason popping off works, but then we don't have a record of the current move
+  const toSlice = Object.keys(MOVEMENTS[movement].moves)
+  const slicedTiles = allTiles.filter(({ key }) => toSlice.includes(key))
+  const unslicedTiles = allTiles.filter(({ key }) => !toSlice.includes(key))
+  const { axis, direction } = MOVEMENTS[movement]
 
-  const props = { cube, allTiles, axis, movement };
+  const props = { cube, allTiles, axis, direction, movement }
 
   return [
     <ConnectedSlice {...props}>{slicedTiles}</ConnectedSlice>,
-    ...unslicedTiles
-  ];
-};
+    ...unslicedTiles,
+  ]
+}
 
-export default renderMove;
+export default renderMove
