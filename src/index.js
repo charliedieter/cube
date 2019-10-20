cd7dfa7ad87da135384b10a62857fd13d05843b0
Charlie Dieter <cdieter@jwplayer.com>
2019-10-20 14:41:02 -0400

    Remove page transitions for now, fix formatting

--- src/index.js
+++ src/index.js
@@ -1,41 +1,33 @@
-import React from "react";
-import { render } from "react-dom";
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { Transition, TransitionGroup } from 'react-transition-group';
-import { Provider } from "react-redux";
-import configureStore from "./redux/store";
+import React from 'react'
+import { render } from 'react-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import configureStore from './redux/store'
 import Home from './pages/Home'
 import Tutorial from './pages/Tutorial'
-import { enter, exit } from './util/pageTransitionUtils'
 
-import "./index.css";
-const store = configureStore();
+import './index.css'
+const store = configureStore()
 
 render(
   <Provider store={store}>
-
     <Router>
-      <Route render={({ location }) => {
-        const { pathname, key } = location;
+      <Route
+        render={({ location }) => {
+          const { pathname, key } = location
 
-        return (
-          <TransitionGroup component={null}>
-            <Transition
-              key={key}
-              appear={true}
-              onEnter={(node, appears) => enter(pathname, node, appears)}
-              onExit={exit}
-              timeout={{ enter: 750, exit: 500 }}
-            >
-              <Switch location={location}>
-                <Route exact path="/" component={Home} />
-                <Route path="/tutorial" component={Tutorial} />
-              </Switch>
-            </Transition>
-          </TransitionGroup>
-        )
-      }} />
+          return (
+            <Switch location={location}>
+              <Route exact path="/" component={Home} />
+              <Route
+                path="/tutorial/:step?"
+                component={({ match: { params } }) => <Tutorial {...params} />}
+              />
+            </Switch>
+          )
+        }}
+      />
     </Router>
   </Provider>,
-  document.getElementById("root")
-);
+  document.getElementById('root'),
+)
