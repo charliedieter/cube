7335a41199044cba2ffe13dfe4f15fd8415e50f7
Charlie Dieter <cdieter@jwplayer.com>
2019-10-20 14:46:24 -0400

    Formatting

--- src/util/cubeUtils.js
+++ src/util/cubeUtils.js
@@ -1,30 +1,30 @@
-import COLORS from "./colors";
-import MOVEMENTS from "./movements";
+import COLORS from './colors'
+import MOVEMENTS from './movements'
 
 export const createTiles = () => {
-  const faces = ["up", "front", "down", "left", "right", "back"];
-  const cols = ["l", "c", "r"];
-  const rows = ["t", "c", "b"];
-  const tiles = {};
+  const faces = ['up', 'front', 'down', 'left', 'right', 'back']
+  const cols = ['l', 'c', 'r']
+  const rows = ['t', 'c', 'b']
+  const tiles = {}
 
   for (let i = 0; i < faces.length; i++) {
     for (let j = 0; j < cols.length; j++) {
       for (let k = 0; k < rows.length; k++) {
-        const position = faces[i][0] + rows[k] + cols[j];
-        const color = COLORS[position[0]];
-        tiles[position] = { position, color };
+        const position = faces[i][0] + rows[k] + cols[j]
+        const color = COLORS[position[0]]
+        tiles[position] = { position, color }
       }
     }
   }
-  return tiles;
-};
+  return tiles
+}
 
 export const setChanges = (cube, direction) => {
-  const move = MOVEMENTS[direction].moves;
+  const move = MOVEMENTS[direction].moves
 
   return Object.values(cube).reduce((newTiles, tile) => {
     tile.position = move[tile.position] ? move[tile.position] : tile.position
-    newTiles[[tile.position]] = tile;
+    newTiles[[tile.position]] = tile
     return newTiles
-  }, {});
-};
+  }, {})
+}
--- src/util/pageTransitionUtils.js
+++ src/util/pageTransitionUtils.js
@@ -1,18 +1,23 @@
-import { TimelineMax as Timeline, Power1 } from 'gsap';
+import { TimelineMax as Timeline, Power1 } from 'gsap'
 
 export const enter = (pathname, node, appears) => {
-  const delay = 0.5;
+  const delay = 0.5
   const timeline = new Timeline({ paused: true })
   timeline
     .from(node, 0, { display: 'none', autoAlpha: 0, delay })
-    .staggerFrom(node.children, 0.375, { autoAlpha: 0, x: -25, ease: Power1.easeOut }, 0.125)
+    .staggerFrom(
+      node.children,
+      0.375,
+      { autoAlpha: 0, x: -25, ease: Power1.easeOut },
+      0.125,
+    )
 
   timeline.play()
 }
 
-export const exit = (node) => {
-  const timeline = new Timeline({ paused: true });
+export const exit = node => {
+  const timeline = new Timeline({ paused: true })
 
-  timeline.to(node, 0.15, { autoAlpha: 0, ease: Power1.easeOut });
-  timeline.play();
+  timeline.to(node, 0.15, { autoAlpha: 0, ease: Power1.easeOut })
+  timeline.play()
 }
